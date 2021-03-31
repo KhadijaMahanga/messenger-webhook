c508a050f84e63fe13d6185977160db52198f6ee
khadija <khadijamahanga@y7mail.com>
2021-03-31 12:58:53 +0300

    webhook verification

--- index.js
+++ index.js
@@ -4,33 +4,45 @@
 const
     express = require('express'),
     bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json()); // creates express http server
+    app = express().use(bodyParser.json());
 
-// Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
 
     let body = req.body;
-
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
-
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
         });
-
-        // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
     } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
     }
 
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            h
+            res.sendStatus(403);
+        }
+    }
 });
